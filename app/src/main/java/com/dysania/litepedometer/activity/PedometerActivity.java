88ae5e5003ecaaa120583074aadca01c9e240b9e
DysaniazzZ <William.fzy@foxmail.com>
2017-05-12 16:50:54 +0800

    the page to show steps count

---
+++ app/src/main/java/com/dysania/litepedometer/activity/PedometerActivity.java
@@ -0,0 +1,23 @@
+package com.dysania.litepedometer.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.dysania.litepedometer.R;
+
+public class PedometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedometer);
+    }
+
+    public void onPedometerStartClick(View view) {
+
+    }
+
+    public void onPedometerStopClick(View view) {
+
+    }
+}
